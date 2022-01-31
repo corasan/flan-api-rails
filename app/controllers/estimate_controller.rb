0b8c60bc0ebd525adfbdf9812e363d841f44e0d0
Henry Paulino <henrypl360@gmail.com>
2022-01-31 13:01:28 +0000

    add some error handling and fix tests

--- app/controllers/estimate_controller.rb
+++ app/controllers/estimate_controller.rb
@@ -2,46 +2,40 @@
 class EstimateController < ApplicationController
   before_action :authorized
 
-  def initialize_estimate
-    @income = user_info.income
-    @checking = user_info.checking
-    @savings = user_info.savings
-    @debt = user_info.debt
-    @rent = user_info.rent
-    @will_save = user_info.will_save
-    @will_pay_debt = will_pay_debt
-    @expenses_total = expenses_total
-  end
-
   def index
-    render json: {} if user_info.nil?
-    initialize_estimate
+    raise ActiveRecord::RecordNotFound if user_info.nil?
 
     data = generate_data
     render json: {
       estimate: date_formatted_estimate(data),
       income_after_expenses: income_after_expenses,
-      will_save: @will_save,
-      will_pay_debt: @will_pay_debt,
+      will_save: user_info.will_save,
+      will_pay_debt: will_pay_debt,
       debt_end: calc_debt_end
     }
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: e.to_s }
   end
 
   def income_after_expenses
-    account_for_debt = @debt <= 0 ? 0 : @will_pay_debt
-    @income - @rent - @expenses_total - account_for_debt - @will_save
+    account_for_debt = user_info.debt <= 0 ? 0 : will_pay_debt
+    user_info.income - user_info.rent - expenses_total - account_for_debt - user_info.will_save
   end
 
   def chart
-    initialize_estimate
+    raise ActiveRecord::RecordNotFound if user_info.nil?
+
     render json: generate_data
+
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: e.to_s }
   end
 
   private
 
   def generate_data
     now = Time.now
-    arr = [{ checking: @checking, savings: @savings, debt: @debt, month: now.month, year: now.year }]
+    arr = [{ checking: user_info.checking, savings: user_info.savings, debt: user_info.debt, month: now.month, year: now.year }]
     (0..range.to_i).each { |x| arr.push(est_object(arr[-1], now.month + x)) }
     arr
   end
@@ -70,26 +64,26 @@ class EstimateController < ApplicationController
   end
 
   def calc_checking(num, debt)
-    account_for_debt = debt <= 0 ? 0 : @will_pay_debt
-    num + @income - @rent - @expenses_total - account_for_debt - @will_save
+    account_for_debt = debt <= 0 ? 0 : will_pay_debt
+    num + user_info.income - user_info.rent - expenses_total - account_for_debt - user_info.will_save
   end
 
   def calc_savings(num)
-    num + @will_save
+    num + user_info.will_save
   end
 
   def calc_debt(num)
-    num - @will_pay_debt
+    num - will_pay_debt
   end
 
   def calc_debt_end
-    return nil unless @debt.positive?
+    return nil unless user_info.debt.positive?
 
     counter = 0
-    amount = @debt
+    amount = user_info.debt
 
     while amount.positive?
-      amount -= @will_pay_debt
+      amount -= will_pay_debt
       counter += 1
     end
     month = Date::MONTHNAMES[counter.month.from_now.month]
