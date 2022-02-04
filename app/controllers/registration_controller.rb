c3142a5c4ccc736aaf5c1884c733d0d31f172cf1
Henry Paulino <henrypl360@gmail.com>
2022-02-04 20:39:53 +0000

    user existence validation

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,12 +3,16 @@ class RegistrationController < ApplicationController
 
   def create
     raise ArgumentError, 'BadRequest Parameter' if payload.blank?
-    @user = User.create!(user_params.merge(uid: payload['sub']))
-    if @user.persisted?
-      UserInfo.create(user_id: @user.id, income: 0, checking: 0, savings: 0, will_save: 0, debt: 0, rent: 0)
-      render json: @user, status: :ok
+    if user_exists?
+      render json: { message: 'User already exists', user: @user }, status: :ok
     else
-      render json: @user.errors, status: :unprocessable_entity
+      @user = User.create!(user_params.merge(uid: payload['sub']))
+      if @user.valid?
+        UserInfo.create(user_id: @user.id, income: 0, checking: 0, savings: 0, will_save: 0, debt: 0, rent: 0)
+        render json: { message: 'User created successfully', user: @user }, status: :ok
+      else
+        render json: { message: 'User not created', errors: @user.errors }, status: :bad_request
+      end
     end
   end
 
@@ -28,5 +32,10 @@ class RegistrationController < ApplicationController
 
   def payload
     @payload ||= FirebaseIdToken::Signature.verify token
+  end
+
+  def user_exists?
+    u = User.find_by(uid: payload['sub'])
+    u.nil? ? false : true
   end
 end
