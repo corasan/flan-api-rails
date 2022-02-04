d4519188a9bcc12ccbf9a4bb2bea77b72d24f173
Henry Paulino <henrypl360@gmail.com>
2022-02-04 22:44:33 +0000

    fix

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,16 +3,12 @@ class RegistrationController < ApplicationController
 
   def create
     raise ArgumentError, 'BadRequest Parameter' if payload.blank?
-    if user_exists?
-      render json: { message: 'User already exists', user: @user }, status: :ok
-    else
       @user = User.create!(user_params.merge(uid: payload['sub']))
-      if @user.valid?
-        UserInfo.create(user_id: @user.id, income: 0, checking: 0, savings: 0, will_save: 0, debt: 0, rent: 0)
-        render json: { message: 'User created successfully', user: @user }, status: :ok
-      else
-        render json: { message: 'User not created', errors: @user.errors }, status: :bad_request
-      end
+    if @user.valid?
+      UserInfo.create(user_id: @user.id, income: 0, checking: 0, savings: 0, will_save: 0, debt: 0, rent: 0)
+      render json: { message: 'User created successfully', user: @user }, status: :ok
+    else
+      render json: { message: 'User not created', errors: @user.errors }, status: :bad_request
     end
   end
 
