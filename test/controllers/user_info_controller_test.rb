dc33877b75e1347a07293112bb2f77f13b13ee28
Henry Paulino <corasan.dev@gmail.com>
2022-08-19 14:07:21 +0100

    update user info test

--- test/controllers/user_info_controller_test.rb
+++ test/controllers/user_info_controller_test.rb
@@ -11,6 +11,11 @@ class UserInfoControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'can update checking and savings' do
+    put '/v2/user_info', params: { checking: 2000, savings: 6000 }, headers: req_headers
+    assert_response :success
+  end
+
   test 'return error message if user_info doesnt exist' do
     get '/v2/user_info', headers: req_headers
     assert_response :success
