f6e1fe7e7ffeca3e44cec5e1469a3d6ac02a4aa1
Henry Paulino <corasan.dev@gmail.com>
2022-09-02 07:42:39 +0100

    add before action

--- app/controllers/V2/default_controller.rb
+++ app/controllers/V2/default_controller.rb
@@ -1,5 +1,7 @@
 module V2
     class DefaultController < ApplicationController
+        skip_before_action :authenticate_user
+
         def index
             render html: "Flan #{ENV['RAILS_ENV']} API V2"
         end
